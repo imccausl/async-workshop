4e39c4525d675282a0891fb49572c2a8b52541f9
Ian McCausland <ian.mccausland@tophat.com>
2025-02-10 13:00:39 -0500

    wip: a simpler solution

--- exercises/exercise-5/src/solution.ts
+++ exercises/exercise-5/src/solution.ts
@@ -1,22 +1,18 @@
 // convert the promise exercise to use async await
-
 function timeout(delay = 1000) {
     return new Promise((resolve) => {
         setTimeout(resolve, delay)
     })
 }
 
-async function count(count: number | string) {
-    console.log(`${count}`)
-    await timeout()
-}
-
 async function countdown() {
+    console.log('3')
     await timeout()
-    await count(2)
-    await count(1)
+    console.log('2')
+    await timeout()
+    console.log('1')
+    await timeout()
     console.log('HAPPY NEW YEAR! 🎉🎉🎉')
 }
 
-console.log('3')
 countdown()
